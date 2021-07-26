bba23e2bda00cb9bd5e3df22881dc42f517c2c3f
Ringo Chan <202669+ringoc@users.noreply.github.com>
2021-07-26 17:33:37 +1000

    add support for loop through all subscription

--- vm-guest-agent/az-check-vm-guest-agent-status.ps1
+++ vm-guest-agent/az-check-vm-guest-agent-status.ps1
@@ -1,14 +1,22 @@
-$results = @(foreach ($vm in $vms = Get-AzVM) {
-    $vmInstanceView = Get-AzVM -Status -ResourceGroup $vm.ResourceGroupName -Name $vm.Name 
-    $agent = $vmInstanceView | Select -ExpandProperty VMAgent | Select VMAgentVersion
-    $status = $vmInstanceView | Select -ExpandProperty Statuses|  Select-Object -Last 1 
+$resultsAll = @(foreach ($subId in $subIds = (Get-AzSubscription | Where-Object {$_.State -ne 'Disabled'}).Id) {
+    $context = Set-AzContext -Subscription $subID
+    $subName = $context.Subscription.Name
+    Write-Host "Working on $SubName"
+    $results = @(foreach ($vm in $vms = Get-AzVM) {
+        $vmInstanceView = Get-AzVM -Status -ResourceGroup $vm.ResourceGroupName -Name $vm.Name 
+        $agent = $vmInstanceView | Select -ExpandProperty VMAgent | Select VMAgentVersion
+        $status = $vmInstanceView | Select -ExpandProperty Statuses|  Select-Object -Last 1 
 
-    [PSCustomObject]@{
-        rgName = $vm.ResourceGroupName
-        location = $vm.Location
-        vmOsType = $vm.StorageProfile.OsDisk.OsType
-        vmOsDiskCreateOption = $vm.StorageProfile.OsDisk.CreateOption
-        vmStatus = $status.Code
-        vmAgentVersion = $agent.VMAgentVersion 
-    }
-}) | Export-Csv -Path .\report.csv
+        [PSCustomObject]@{
+            subID = $subID
+            subName = $subName
+            vmName = $vm.Name
+            rgName = $vm.ResourceGroupName
+            location = $vm.Location
+            vmOsType = $vm.StorageProfile.OsDisk.OsType
+            vmOsDiskCreateOption = $vm.StorageProfile.OsDisk.CreateOption
+            vmStatus = $status.Code
+            vmAgentVersion = $agent.VMAgentVersion 
+        }
+    }) | Export-Csv -Path .\report.csv -NoTypeInformation -Append
+})
